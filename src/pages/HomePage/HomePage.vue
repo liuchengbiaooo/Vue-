ee14df2724c1f10ce36b82f45ba4822f355eda4b
liuchengbiao <29648397@qq.com>
2018-12-04 21:48:45 +0800

    第一天结束

--- src/pages/HomePage/HomePage.vue
+++ src/pages/HomePage/HomePage.vue
@@ -1,232 +1,296 @@
 <template>
-  <div class="g-row">
-    <div class="m-indexHd">
-      <div class="line">
-        <a href="#" class="logo"></a>
-        <div class="m-topSearchIpt ipt">
-          <i class="icon"></i>
-          <span class="placeholder">
-                搜索商品, 共19915款好物
-              </span>
+  <div>
+    <HomeHeader/>
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <img
+            src="https://yanxuan.nosdn.127.net/850c005113e5becbfd98d797b8a2101a.jpg?imageView&quality=75&thumbnail=750x0">
         </div>
-        <div class="loginBtn">登录</div>
+        <div class="swiper-slide">
+          <img
+            src="https://yanxuan.nosdn.127.net/1d66a50452354fa28b045853c078cbe3.jpg?imageView&quality=75&thumbnail=750x0">
+        </div>
+        <div class="swiper-slide">
+          <img
+            src="https://yanxuan.nosdn.127.net/e0c3e253dcfdb170bda823ac5b71247e.jpg?imageView&quality=75&thumbnail=750x0">
+        </div>
+        <div class="swiper-slide">
+          <img
+            src="https://yanxuan.nosdn.127.net/850c005113e5becbfd98d797b8a2101a.jpg?imageView&quality=75&thumbnail=750x0">
+        </div>
+        <div class="swiper-slide">
+          <img
+            src="https://yanxuan.nosdn.127.net/fda101d5e6beeb56d0ff8245139ff30b.jpg?imageView&quality=75&thumbnail=750x0">
+        </div>
+        <div class="swiper-slide">
+          <img
+            src="https://yanxuan.nosdn.127.net/018b7902a3fb1e48dbdd8b7bb42674aa.jpg?watermark&type=1&gravity=northwest&dx=0&dy=0&image=YTRhMmIzYmI1Y2I1YTc4ZGM5MmE3Nzk2NzAwYTFiNjgucG5n|imageView&quality=75&thumbnail=750x0">
+        </div>
+        <div class="swiper-slide">
+          <img
+            src="https://yanxuan.nosdn.127.net/5a2289149a59ca24ef2256674542e4cb.jpg?imageView&quality=75&thumbnail=750x0">
+        </div>
       </div>
-      <div class="tabWrap">
-        <div class="m-tabs scroll">
-          <header>
-            <div class="inner">
-              <div class="list">
-                <div class="tab active">
-                        <span class="txt">
-                          推荐
-                        </span>
-                </div>
-                <div class="tab">
-                        <span class="txt">
-                          居家
-                        </span>
-                </div>
-                <div class="tab">
-                        <span class="txt">
-                          鞋包配饰
-                        </span>
-                </div>
-                <div class="tab">
-                        <span class="txt">
-                          服装
-                        </span>
-                </div>
-                <div class="tab">
-                        <span class="txt">
-                          电器
-                        </span>
-                </div>
-                <div class="tab">
-                        <span class="txt">
-                          洗护
-                        </span>
-                </div>
-                <div class="tab">
-                        <span class="txt">
-                          饮食
-                        </span>
-                </div>
-                <div class="tab">
-                        <span class="txt">
-                          餐厨
-                        </span>
-                </div>
-                <div class="tab">
-                        <span class="txt">
-                          婴童
-                        </span>
-                </div>
-                <div class="tab">
-                        <span class="txt">
-                          文体
-                        </span>
-                </div>
-                <div class="tab">
-                        <span class="txt">
-                          特色区
-                        </span>
-                </div>
-              </div>
-            </div>
-          </header>
-        </div>
-        <div class="toggleWrap">
-          <div class="linear"></div>
-          <div class="toggle">
-            <div class="icon"></div>
+      <!-- 如果需要分页器 -->
+      <div class="swiper-pagination"></div>
+      <!-- 如果需要滚动条 -->
+      <div class="swiper-scrollbar"></div>
+    </div>
+    <div class="m-indexServicePolicy active">
+      <ul class="g-grow">
+        <li class="item">
+          <a>
+            <i class="u-icon u-icon-servicePolicy-index"
+               style="background-image:url(http://yanxuan.nosdn.127.net/cae45612b8aae577d8bd73338e2fc02c.png);"></i>
+            <span class="text">
+                网易自营品牌
+            </span>
+          </a>
+        </li>
+        <li class="item">
+          <a>
+            <i class="u-icon u-icon-servicePolicy-index"
+               style="background-image:url(http://yanxuan.nosdn.127.net/cae45612b8aae577d8bd73338e2fc02c.png);"></i>
+            <span class="text">
+                30天无忧退货
+            </span>
+          </a>
+        </li>
+        <li class="item">
+          <a>
+            <i class="u-icon u-icon-servicePolicy-index"
+               style="background-image:url(http://yanxuan.nosdn.127.net/cae45612b8aae577d8bd73338e2fc02c.png);"></i>
+            <span class="text">
+                48小时快速退款
+            </span>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="container">
+      <div class="wrapper">
+        <div class="list">
+          <div class="icon">
+            <img src="http://yanxuan.nosdn.127.net/98b6a6fc32f1fea861934816729e2cf5.png"/>
           </div>
+          <span class="txt">居家</span>
         </div>
+        <div class="list">
+          <div class="icon"><img src="http://yanxuan.nosdn.127.net/46d33b9a9fbb659fcbac37ec58d51e62.png"/></div>
+          <span class="txt">鞋包配饰</span>
+        </div>
+        <div class="list">
+          <div class="icon"><img src="http://yanxuan.nosdn.127.net/31831ada59dc10319cba195620ed9ed0.png"/></div>
+          <span class="txt">服装</span>
+        </div>
+        <div class="list">
+          <div class="icon"><img src="http://yanxuan.nosdn.127.net/45833c71d4b0d0de0755a20f893fa25f.png"/></div>
+          <span class="txt">电器</span>
+        </div>
+        <div class="list">
+          <div class="icon"><img src="http://yanxuan.nosdn.127.net/2fde77529e90a26427d1c02faa3bfbf6.png"/></div>
+          <span class="txt">婴童</span>
+        </div>
+        <div class="list">
+          <div class="icon"><img src="http://yanxuan.nosdn.127.net/fc3e359da08577228354da61ea912c99.png"/></div>
+          <span class="txt">餐厨</span>
+        </div>
+        <div class="list">
+          <div class="icon"><img src="http://yanxuan.nosdn.127.net/15e364ca93313bbd6e87dfcba7ae7b74.png"/></div>
+          <span class="txt">饮食</span>
+        </div>
+        <div class="list">
+          <div class="icon"><img src="http://yanxuan.nosdn.127.net/793bca13bb931475ea7f0c00299362bb.png"/></div>
+          <span class="txt">洗护</span>
+        </div>
+        <div class="list">
+          <div class="icon"><img src="http://yanxuan.nosdn.127.net/97eb6fd2c7ea76a3a42b9dafa3bd6543.png"/></div>
+          <span class="txt">文体</span>
+        </div>
+        <div class="list">
+          <div class="icon"><img src="http://yanxuan.nosdn.127.net/db5e2ce8c66f7db3f4282ecb24a64236.png"/></div>
+          <span class="txt">超级会员</span>
+        </div>
+      </div>
+    </div>
+    <div class="m-lazyload img m-lazyload-loaded">
+      <img class="swiper-lazy" src="https://yanxuan.nosdn.127.net/15435895031254332.gif">
+    </div>
+    <div class="onimg">
+      <div>
+        <img src="https://yanxuan.nosdn.127.net/15439062209423716.png"/>
+        <img src="https://yanxuan.nosdn.127.net/15439062418853717.png"/>
+      </div>
+    </div>
+    <div class="onimg">
+      <div>
+        <img src="https://yanxuan.nosdn.127.net/15438948939222563.png"/>
+        <img src="https://yanxuan.nosdn.127.net/15439062784443718.png"/>
+      </div>
+    </div>
+    <div class="m-bossRecommendModule active">
+      <img src="https://yanxuan.nosdn.127.net/b36b6e2c6132b5980c047005f7520795.png">
+    </div>
+    <div class="m-sceneLightShoppingGuideModule active">
+      <div>
+        <a class="m-styleBanner">
+          <div class="title">
+            12.12爱吃福利社
+          </div>
+          <div class="desc">
+            暖冬美食荟萃
+          </div>
+          <div class="picList">
+            <img class=".pic" src="http://yanxuan.nosdn.127.net/c24d528dfc4c043cad7cdb560d2b7764.png">
+            <img class=".pic" src="http://yanxuan.nosdn.127.net/d36dc48ac4456816e60f1592f02e034d.png"/>
+          </div>
+        </a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  import HomeHeader from "../../components/HomeHeader/HomeHeader.vue"
+  import Swiper from 'swiper'
+  import 'swiper/dist/css/swiper.min.css'
+
   export default {
-    name: "home-page"
+    name: "home-page",
+    mounted() {
+      new Swiper('.swiper-container', {
+        //direction: 'vertical', // 垂直切换选项
+        loop: true, // 循环模式选项
+        // 如果需要分页器
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        autoplay: {
+          delay: 3000, //循环时间
+        }
+      })
+    },
+    components: {
+      HomeHeader,
+    }
   }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  .g-row
-    width 10rem
-    margin-right auto
-    margin-left auto
-    .m-indexHd
-      background-color #fff
-      border-bottom 1px solid #d9d9d9
-      .line
-        position relative
-        z-index 2
-        display flex
-        flex-flow row nowrap
-        -webkit-box-align center
-        align-items center
-        background #fff
-        padding .21333rem .4rem
-        & > .ipt
-          -webkit-box-flex 1
-          flex-grow 1
-        .logo
-          width 1.84rem
-          height .53333rem
-          background-image url("//yanxuan-static.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/icon-normal/indexLogo-11d65342f9.png")
-          display inline-block
-          margin-right .26667rem
-          background-size cover
-          background-position center
-        .m-topSearchIpt
-          display flex
-          flex-flow row nowrap
-          -webkit-box-align center
-          align-items center
-          -webkit-box-pack center
-          justify-content center
-          height .74667rem
-          font-size .37333rem
-          background-color #ededed
-          border-radius .10667rem
-          .icon
-            display inline-block
-            vertical-align middle
-            background-image url("//yanxuan-static.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/icon-normal/search2-2fb94833aa.png")
-            background-repeat no-repeat
-            background-size 100% 100%
-            width .37333rem
-            height .37333rem
-            margin-right .13333rem
-          .span
-            color #666
+  .swiper-container
+    width: 100%
+    height: 100%
+    img
+      width 100%
+      height auto
 
-        .loginBtn
-          width .98667rem
-          height .53333rem
-          line-height .53333rem
-          text-align center
-          color #b4282d
-          border 1px solid #b4282d
-          border-radius .10667rem
-          margin-left .21333rem;
-    .tabWrap
-      position relative
-      z-index 2
-      background #fff
-      margin-top -.01333rem
-      .m-tabs
-        padding-right 1.33333rem
-        width 100%
-        >header
-          display flex
-          flex-flow row nowrap
-          background-color #fff
-          position relative
-          .inner
-            display flex
-            flex-flow row nowrap
-            .list
-              display flex
-              flex-flow row nowrap
-              flex-shrink 0
-              padding 0 .4rem
-              background #fff
-              .tab
-                margin-left .26667rem
-                flex-shrink 0
-                position relative
-                &.active
-                  color red
-                  .txt
-                    position relative
-                    color #b4282d
-                    &:after
-                      content: ' ';
-                      position: absolute;
-                      left: 0;
-                      bottom: 0;
-                      width: 100%;
-                      height: .05333rem;
-                      background-color: #b4282d;
-                .txt
-                  display: inline-block;
-                  padding: 0 .21333rem;
-                  line-height: .8rem;
-                  font-size: .37333rem;
-                  color: #333;
-                  text-align: center;
-              .tab:first-of-type
-                margin-left 0
-    .toggleWrap
+  .m-indexServicePolicy
+    background: #FFEDDB;
+    .g-grow
+      width: 100%;
+      height: .96rem;
+      padding: 0 .2rem;
       display flex
-      -webkit-box-flex: 0;
-      flex-grow: 0;
+      flex-flow: row nowrap;
       -webkit-box-align: center;
       align-items: center;
-      -webkit-box-pack: center;
-      justify-content: center;
-      position: absolute;
-      top: 0;
-      right: 0;
-      .linear
-        width: .8rem;
-        height: .8rem;
-        background-image: linear-gradient(to right,rgba(255,255,255,0) 0,#fff 100%);
-      .toggle
-        width: 1.33333rem;
-        height: .8rem;
-        text-align: center;
-        background: #fff;
-        .icon
-          margin-top: .2rem;
+      zoom: 1;
+      .item
+        -webkit-box-flex: 1;
+        flex: 1;
+        float: left;
+        a
           display: inline-block;
           vertical-align: middle;
-          background-repeat: no-repeat;
-          background-size: 100% 100%;
-          width: .4rem;
-          height: .4rem;
-          background-image url("//yanxuan-static.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/icon-normal/arrow-down-3-a6045aadfd.png")
+          height: .42667rem;
+          .u-icon-servicePolicy-index
+            display: inline-block;
+            vertical-align: middle;
+            background-repeat: no-repeat;
+            background-size: 100% 100%;
+            width: .42667rem;
+            height: .42667rem;
+          .text
+            color: #B4282D;
+            font-size: .32rem;
+            margin-left: .05rem;
+            line-height: .42667rem;
+            display: inline-block;
+            vertical-align: middle;
+  .container
+    padding-bottom: .69333rem;
+    margin-left: auto;
+    margin-right: auto;
+    position: relative;
+    overflow: hidden;
+    z-index: 1;
+    background: #FFEDDB;
+    .wrapper
+      height 3.4rem
+      flex-shrink: 0;
+      width: 100%;
+      position: relative;
+      display flex
+      flex-wrap wrap
+      justify-content space-evenly
+      .list
+        width 1.3rem
+        height 1.6rem
+        margin 0.1rem 0.2rem
+        text-align center
+        .icon
+          img
+            width: 1.1rem
+            height: 1.1rem
+        .txt
+          font-size: .32rem;
+  .m-lazyload
+    background 0 0
+    height: 100%;
+    .swiper-lazy
+      width  100%
+      height auto
+
+  .onimg
+    width 100%
+    height auto
+    display: inline
+    img
+      width 49%
+      height atuo
+  .m-bossRecommendModule
+    width 100%
+    height auto
+    img
+      width 100%
+      height auto
+  .m-sceneLightShoppingGuideModule
+    background: #B7030A;
+    overflow: hidden;
+    padding: .13333rem .4rem .4rem .34667rem;
+    margin-bottom: .26667rem;
+    .m-styleBanner
+      background-color: #F5F5F5;
+      display: block;
+      width: 4.57333rem;
+      height: 3.52rem;
+      float: left;
+      margin: .05333rem 0 0 .05333rem;
+      padding: .26667rem 0 0 .26667rem;
+      border-radius: .05333rem;
+      .title
+        padding-left: .13333rem;
+        font-size: .42667rem;
+        font-family: PingFangSC-Medium;
+      .desc
+        padding-left: .13333rem;
+        font-size: .32rem;
+      .picList
+        width 100%
+        .pic
+          width: 2rem;
+          height: 2rem;
+          float: left;
+          margin-right: .05333rem;
 </style>
